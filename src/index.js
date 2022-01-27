cc222713832fc276c93df630e5c9921950a3cef5
Leonardo de Souza Nunes <contato.leonunez@gmail.com>
2022-01-27 00:58:02 -0300

    Allow post and get on /participants with username, introduction message and giving a list of users

--- src/index.js
+++ src/index.js
@@ -1,8 +1,68 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import dayjs from "dayjs";
 
-const app = express()
-app.use(cors())
+dotenv.config();
+const app = express();
 
+app.use(express.json());
+app.use(cors());
 
-app.listen(4000, () => console.log('Running at http://localhost:4000'))
+async function connectToCollection(collectionName) {
+    const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+    try {
+        const connection = await mongoClient.connect();
+        const collection = connection.db("api-batepapo-uol").collection(collectionName);
+
+        return [connection, collection];
+    } catch {
+        console.log("connectMongo has some issues...");
+    }
+}
+
+app.post("/participants", async (req, res) => {
+    const participant = { name: req.body.name, lastStatus: Date.now() };
+    const introMessage = {
+        from: req.body.name,
+        to: "Todos",
+        text: "entra na sala...",
+        type: "status",
+        time: dayjs().format("HH:mm:ss"),
+    };
+    const [connectionParticipant, collectionParticipant] = await connectToCollection(
+        "participants"
+    );
+    const [connectionMessage, collectionMessage] = await connectToCollection("messages");
+
+    try {
+        await collectionMessage.insertOne(introMessage);
+        await collectionParticipant.insertOne(participant);
+        res.sendStatus(201);
+        connectionParticipant.close();
+        connectionMessage.close();
+    } catch {
+        console.log("Error at the POST in /participants route");
+        connectionParticipant.close();
+        connectionMessage.close();
+    }
+});
+
+app.get("/participants", async (req, res) => {
+    const [connectionParticipants, collectionParticipants] = await connectToCollection(
+        "participants"
+    );
+
+    try {
+        const participants = await collectionParticipants.find({}).toArray();
+        res.send(participants);
+        connectionParticipants.close();
+    } catch {
+        console.log("Error at the GET in /participants route");
+        connectionParticipants.close();
+    }
+});
+
+app.listen(5000, () => console.log("Running at http://localhost:5000"));
